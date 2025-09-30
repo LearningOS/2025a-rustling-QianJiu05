ea98d0869233396344dd945c544403fa2c7a09b3
QianJiu05 <3211471056@qq.com>
2025-10-01 00:55:03 +0800

    from into

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -44,6 +44,27 @@ impl Default for Person {
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+
+        let parts: Vec<&str> = s.split(',').collect();
+        if parts.len() != 2{
+            return Person::default();
+        }
+
+        let name = parts[0];
+        if name.is_empty() || parts[1].is_empty() {
+            return Person::default();
+        }
+
+        match parts[1].parse::<usize>() {
+            Ok(age) => Person {
+                name: String::from(name),
+                age
+            },
+            Err(_) => Person::default()
+        }
     }
 }
 
